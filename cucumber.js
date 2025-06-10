f829cab2b40d72f35edfb0ab004027e065dd1b4c
SreekanthQE <159225385+SreekanthQE@users.noreply.github.com>
2025-06-10 11:38:51 +0530

    final

--- cucumber.js
+++ cucumber.js
@@ -1,23 +1,23 @@
 // cucumber.js
-   import { resolve } from 'path';
+import { resolve } from 'path';
 
-   const config = {
-     paths: [resolve('./src/features/**/*.feature')],
-     require: [
-       resolve('./src/support/hooks.js'), // hooks.js must be first!
-       resolve('./src/step_definitions/**/*.js'),
-       resolve('./src/support/**/*.js')
-     ],
-     format: [
-      'html:reports/report.html',
-      'summary',
-      'allure-cucumberjs/reporter' // ✅ THIS is the correct formatter
-    ],
-    formatOptions: {
-      allure: {
-        resultsDir: 'allure-results'
-      }
+const config = {
+  paths: [resolve('./src/features/**/*.feature')],
+  import: [
+    resolve('./src/support/hooks.js'), // hooks.js must be first!
+    resolve('./src/step_definitions/**/*.js'),
+    resolve('./src/support/**/*.js')
+  ],
+  format: [
+    'html:reports/report.html',
+    'summary',
+    'allure-cucumberjs/reporter' // ✅ THIS is the correct formatter
+  ],
+  formatOptions: {
+    allure: {
+      resultsDir: 'allure-results'
     }
-   };
+  }
+};
 
-   export default config;
+export default config;
